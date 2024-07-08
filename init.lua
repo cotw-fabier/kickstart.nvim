e4e0a4a767e3e743a54a20ca901b84dd6c43df6f
Dan Kolansky <Dan@championsoftheweb.com>
2024-07-08 10:35:28 -0400

    Update theme and remove LSPs since they don't appear to be working

--- init.lua
+++ init.lua
@@ -567,10 +567,10 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        pyright = {},
-        rust_analyzer = {},
-        dartls = {},
-        tsserver = {},
+        -- pyright = {},
+        -- rust_analyzer = {},
+        -- dartls[ = {},
+        -- tsserver = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -780,13 +780,16 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    'catppuccin/nvim',
     'folke/tokyonight.nvim',
+    'EdenEast/nightfox.nvim',
+    'rebelot/kanagawa.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'kanagawa-wave'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
